52a962c7ede8bb76e827c8fd87f748e6df734ada
satellitex <s.a.t.e.3.ths@gmail.com>
2018-07-06 17:58:26 +0900

    add submodule install pipenv

---
+++ deeplearning/loader/loader.py
@@ -0,0 +1,20 @@
+# coding: utf-8
+
+import json
+
+class Loader:
+
+    def __init__(self):
+        pass
+
+    def load(self):
+        pass
+
+    def next(self):
+        pass
+
+    def get_train_data(self):
+        pass
+
+    def get_test_data(self):
+        pass
